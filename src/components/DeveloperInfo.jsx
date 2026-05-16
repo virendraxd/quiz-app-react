1342dbaa5c404d23d4317425330a481127e1b45d
virendraxd <shakyaraju605@gmail.com>
2026-05-16 22:53:57 +0530

    img changes

--- src/components/DeveloperInfo.jsx
+++ src/components/DeveloperInfo.jsx
@@ -6,7 +6,7 @@ function DeveloperInfo() {
                 target="_blank"
                 rel="noopener noreferrer"
             >
-                <img src="src/assets/favicon.svg" alt="virendra-portfolio-logo" />
+                <img src="public/favicon.svg" alt="virendra-portfolio-logo" />
                 <div className="middle">
                     <p>Developed by
                         <span> Virendra Singh </span>
